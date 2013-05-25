b8c06d0a5261670da210fafb92f073a550a11ef3
gestapolur <gestapolur@gmail.com>
2013-05-25 17:13:31 +0800

    add regex

--- wordparser.py
+++ wordparser.py
@@ -2,6 +2,7 @@
 import jinja2
 import sys
 import os
+import re
 import cgi
 import datetime
 import urllib
@@ -17,6 +18,8 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 FETCH_TWEET_CNT = 5
 
 FETCH_WORD_RANK_CNT = 10
+
+HTTP_SUB_REGEX = re.compile('(https?://[\S]+)')
 
 class Words(db.Model):
     word = db.StringProperty()
@@ -44,6 +47,7 @@ class AnalyzeTweets(webapp2.RequestHandler):
     else display no such user
     or that user locked his tweets
     """
+    #TODO if it not use DB could be faster, not use it.
     def post(self):
 
         user_name = self.request.get('username', FETCH_TWEET_CNT )
@@ -51,19 +55,22 @@ class AnalyzeTweets(webapp2.RequestHandler):
         tweets_entites = TweetsFetcher.request_timeline.get_tweets( user_name, FETCH_TWEET_CNT )
         
         for tweet in tweets_entites:
-            tweet_word_list = list( jieba.cut( tweet[ 'text' ] ) )
-            #words in tweet_word_list should check to ensure they're all Chinese words
-            for w in tweet_word_list:
-                if sys.getsizeof( w ) > 32: # more than two zh char
-                    #word_data = Words.get(keys=w)
-                    word_data = Words.gql("WHERE word = :1", w).get()
-                    #print word_data.__class__
-                    if word_data == None:
-                        word_data = Words( get_word_key( w ) )
-                        word_data.word = w
-                    else:
-                        word_data.count = word_data.count + 1
-                    word_data.put()
+            if (tweet[ 'lang' ] == 'zh') or (tweet[ 'lang' ] == 'jp'):
+            # some traditional Chinese tweet may be detected as japanese
+                tweet_text = HTTP_SUB_REGEX.sub( '', tweet[ 'text' ] )
+                tweet_word_list = list( jieba.cut( tweet_text ) )
+                #words in tweet_word_list should check to ensure they're all Chinese words
+                for w in tweet_word_list:
+                    if sys.getsizeof( w ) > 32: # more than two zh char
+                        #word_data = Words.get(keys=w)
+                        word_data = Words.gql("WHERE word = :1", w).get()
+                        #print word_data.__class__
+                        if word_data == None:
+                            word_data = Words( get_word_key( w ) )
+                            word_data.word = w
+                        else:
+                            word_data.count = word_data.count + 1
+                        word_data.put()
 
         #redirect
         self.redirect('/')
