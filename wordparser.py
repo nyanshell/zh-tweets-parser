fcf8045a235844aa803683b12473896617b1d409
gestapolur <gestapolur@gmail.com>
2013-05-29 22:05:24 +0800

    little fix

--- wordparser.py
+++ wordparser.py
@@ -1,4 +1,20 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#
+# Copyright 2013 Gestalt Lur.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 import jinja2
 import sys
 import os
@@ -24,6 +40,7 @@ HTTP_SUB_REGEX = re.compile('(https?://[\S]+)')
 EN_WORDS_SUB_REGEX = re.compile('(\w+)')
 
 class Words(db.Model):
+    runtime = db.StringProperty()
     word = db.StringProperty()
     count = db.IntegerProperty(default=1 )
 
